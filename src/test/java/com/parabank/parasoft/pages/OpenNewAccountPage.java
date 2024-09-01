dd28f00fbb50614ac536c6253e6af86b1384ceb3
samman.zim <zimadro@gmail.com>
2024-09-01 09:42:39 +0600

    continue

--- src/test/java/com/parabank/parasoft/pages/OpenNewAccountPage.java
+++ src/test/java/com/parabank/parasoft/pages/OpenNewAccountPage.java
@@ -4,27 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class OpenNewAccountPage extends BasePage{
+public class OpenNewAccountPage extends BasePage {
     public OpenNewAccountPage(WebDriver driver) {
         super(driver);
     }
 
-    public OpenNewAccountPage selectAccountType(int index){
-    new Select(getWebElement(By.id("type"))).selectByIndex(index);
+    public OpenNewAccountPage selectAccountType(int index) {
+        new Select(getWebElement(By.id("type"))).selectByIndex(index);
         return this;
-
     }
-    public OpenNewAccountPage selectAccountNumber(int index){
+
+    public OpenNewAccountPage selectAccountNumber(int index) {
         new Select(getWebElement(By.id("fromAccountId"))).selectByIndex(index);
         return this;
-
     }
-    public OpenNewAccountPage clickNewAccountBtn(){
-        getWebElement(By.xpath("//input[@value='Open New Account']")).click();
+
+    public OpenNewAccountPage clickNewAccountBtn() {
+        getWebElement(By.cssSelector("input[value='Open New Account']")).click();
         return this;
+    }
 
+//    public boolean hasAccountId() {
+//        return getWebElements(By.id("newAccountId")).size() > 0;
+//    }
 }
-    public boolean hasAccountID(){
-        return getWebElenements(By.id("newAccountId")).size()>0;
-    }
-    }
